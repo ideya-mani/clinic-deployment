f2afc7b2b4a41ad388a04a7d5d55c7ba96139b0d
ideya-mani <airtelmani26@gmail.com>
2025-02-03 17:24:51 +0530

    Fix Vercel routing issue

---
+++ server/dist/models/Contact.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+// Define the contact schema
+const contactSchema = new mongoose_1.default.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+    subject: { type: String, required: true },
+    message: { type: String, required: true },
+}, { timestamps: true }); // To automatically include createdAt and updatedAt
+// Create the model
+const Contact = mongoose_1.default.model('Contact', contactSchema);
+exports.default = Contact;
---
+++ server/dist/server.js
@@ -0,0 +1,36 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const patientRoutes_1 = __importDefault(require("./routes/patientRoutes"));
+const adminRoutes_1 = __importDefault(require("./routes/adminRoutes"));
+const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
+// import timelineRoutes from './routes/timelineRoutes';
+const postRoutes_1 = __importDefault(require("./routes/postRoutes"));
+const contactRoutes_1 = __importDefault(require("./routes/contactRoutes"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT || 5000;
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+// MongoDB connection
+mongoose_1.default.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/medical_appointments')
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log('Error connecting to MongoDB:', err));
+// Routes
+app.use('/patient', patientRoutes_1.default);
+app.use('/admin', adminRoutes_1.default);
+app.use('/auth', authRoutes_1.default);
+// app.use('/api/timeline', timelineRoutes);
+app.use('/api/post', postRoutes_1.default);
+app.use('/patient', contactRoutes_1.default);
+// ✅ Always listen on the port, whether in production or development
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+exports.default = app;
